1ac6f86eee95ad031c4d0c265e87e5c70b5b895d
muhammad-ameen356 <mameen@nisum.com>
2023-09-28 18:04:30 +0500

    API in next.js

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "./schema";
 
 // //* for prevent caching we use request: NextRequest
 export function GET(request: NextRequest) {
@@ -17,5 +18,21 @@ export function GET(request: NextRequest) {
 export async function POST(request: NextRequest) {
   const body = await request.json();
 
+  if (!body.name)
+    return NextResponse.json({ message: "Invalid Name" }, { status: 400 });
+
   return NextResponse.json(body);
+}
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: Number } }
+) {
+  const body = await request.json();
+  const validation = schema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  }
+
+  return NextResponse.json({ message: "Success" });
 }
